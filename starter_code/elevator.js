a5de59ee27cf7c812b3e55c2b553e0a30d6dd9ac
Cristina Bertrand <cristina@11870.com>
2018-01-06 19:15:19 +0100

    feat: everything until iteration 3

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,70 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "";
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  /*
+      To keep a track of everyone, you will have three different lists:
+
+      waitingList: people waiting for the elevator -they made the request and they're waiting the elevator to come
+      passengers: people currently in the elevator
+      requests: a list of pending requests. Floors where the elevator must stop.
+
+  */
+
+  start() {
+    var moveElevator = setInterval(this.update, 1000);
+  }
+
+  stop() {
+    clearInterval(moveElevator);
+  }
+
+  update() {
+    this.log();
+  }
+
+    _passengersEnter(person) {
+    this.passengers.push(person);
+    this.waitingList.splice(this.waitingList.indexOf(person), 1);
+    console.log(`${person.name} entered in floor ${person.originFloor} with destination ${person.destinationFloor}`);
+}
+
+    _passengersLeave() {
+    this.passengers.pop(person);
+    console.log(`${person.name} has left the elevator`)
+}
+
+
+  floorUp() {
+    this.floor < 10
+    ? (this.floor++, this.direction = 'Up')
+    : console.log("The elevator is in the last floor");
+    }
+
+  floorDown() {
+    this.floor > 0
+      ? (this.floor--, this.direction = 'Down')
+      : console.log("The elevator is in the first floor");
+    }
+
+    call() {
+//When a person calls the elevator (the call function is executed), we will add that person into the waitingList array.
+      this.waitingList.push(person);
+//Also, add the (originFloor) to the requests array to let the elevator know where it has to stop to pick the passenger up.
+      this.requests.push(person.originFloor);
+    }
+
+
+  log() {
+    console.log(`Direction: ${this.direction}`);
+    console.log(`Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,21 @@
 const Elevator = require('./elevator.js');
+
+let elevator = new Elevator();
+let person = new Person();
+
+//Recomiendo q en el index.js hagais un array de N personas y llameis N veces al call
+//La primera posición del array de requests determina hacia que planta debe ir el ascensor! Y tendra q pararse en las plantas q le hayan llamado x el camino (x ejemplo) para recoger personas y dejar
+//Pero luego las personas van llamando a call segun pasa el tiempo
+//Para darle mas vidilla podéis hacer que cada X segundos una persona aleatoria llame al ascensor, para los mas hackers
+//Primer reto: que Manolita llame al ascensor en la planta 4 y cuando se monte diga q quiere ir a la 10
+//El ascensor tendra q ir a la 4 y luego a la 10
+//Luego tendra que quedarse en la 10 o bajar al 0 (según vuestro plan de ahorro energético)
+
+
+persons = [Maria, Luis, Iñigo, Paloma, Iñaki];
+
+
+elevator.log();
+elevator.update();
+elevator.floorDown();
+elevator.log();
